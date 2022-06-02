74d9e856c9a8674537bb24a8373fcfefd5fc6a64
Abel <abeljeb@gmail.com>
2022-06-02 08:47:23 -0700

    commiting hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+/*this program prints good stuff about the awesomness of programming*/
 int main(void)
 {
-  puts("\"Programing is like building a multilingual puzzel\n");
-  return (0);
+puts("\"Programing is like building a multilingual puzzle\n");
+return (0);
 }
