365555e852af333588db2940bb68bf69dd0495c1
Abel <abeljeb@gmail.com>
2022-06-02 09:02:25 -0700

    commiting hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /*
  * main - entrypoint
- * Return: Always 0 (Success)*/
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
