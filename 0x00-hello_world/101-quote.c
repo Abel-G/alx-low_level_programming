d85b0156dc6906a57d72d0768e076e1bfb633d1a
Abel <abeljeb@gmail.com>
2022-06-02 17:15:11 -0700

    commit stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*Main - Entry Point
+*Return : Always 1 (failure)
+*/
+int main(void)
+{
+fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
