f4ee84b96c09c9e40eefaf26e9afe377cc73b498
Abel <abeljeb@gmail.com>
2022-06-02 18:21:38 -0700

    commit stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
 */
 int main(void)
 {
-  fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",1);
+  fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",1);
 return (1);
 }
