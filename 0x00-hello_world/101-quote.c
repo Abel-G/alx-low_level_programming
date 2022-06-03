346d21f5cdc4cc7855bbbe8bca1e302990b5ed4b
Abel <abeljeb@gmail.com>
2022-06-02 17:47:30 -0700

    commiting stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 #include <string.h>
 #include <unistd.h>
 /**
-*Main - Entry Point
-*Return : Always 1 (failure)
+*main - Entry Point
+*Return: Always 1 (failure)
 */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 return (1);
 }
