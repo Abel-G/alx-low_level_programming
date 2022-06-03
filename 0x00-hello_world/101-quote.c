f5a646b2d399f7c86eef263fe9d73151c6c02976
Abel <abeljeb@gmail.com>
2022-06-02 18:36:18 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/**
+/*
 *main - Entry Point
 *Return: Always 1 (failure)
 */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
