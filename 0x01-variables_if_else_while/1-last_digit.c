8fadbc7fe3b2e9d76294464c2c14ce9ed80ca973
Abel <abeljeb@gmail.com>
2022-06-04 04:39:45 -0700

    commiting the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ else if (m != 0 && m < 6)
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 }
 else
-{  
+{
 printf("Last digit of %d is %d and is 0\n", n, m);
 }
 return (0);
