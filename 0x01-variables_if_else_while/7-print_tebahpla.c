fad2ffbfc885a9576de630594962d7993ca4f548
Abel <abeljeb@gmail.com>
2022-06-04 07:45:57 -0700

    printing lower case alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+* Description: printing alphabets in reverse
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int i;
+char c;
+for (i = 122; i >= 97; i--)
+{
+c = (char)i;
+putchar(i);
+}
+return (0);
+}
