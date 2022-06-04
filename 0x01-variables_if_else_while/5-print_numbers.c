68f8543d45ef559c0387468c3dc84701b32ad1b4
Abel <abeljeb@gmail.com>
2022-06-04 06:56:09 -0700

    printing intiger numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+* Description: printing base 10 numbers
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i);
+}
+putchar('\n');
+}
