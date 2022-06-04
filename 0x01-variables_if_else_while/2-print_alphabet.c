07409032f6be796211153b6a2fc4e5d1596873b7
Abel <abeljeb@gmail.com>
2022-06-04 05:20:44 -0700

    commiting lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+* Description: printing alphabets in lowercase
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+for (int i =97; i <= 122; i++)
+char c = (char)i;
+putchar(c);
+}
+return (0);
+}
