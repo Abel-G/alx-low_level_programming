84cbbc3fce95ec75ef58a135166d0c21b803f7e2
Abel <abeljeb@gmail.com>
2022-06-04 09:51:38 -0700

    two digit combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,10 +10,10 @@ int main(void)
 int i, j;
 for (i = 0; i < 9; i++)
 {
-for (j = 0; j < i; j++)
+for (j = 0; j <= 9; j++)
 {
-putchar(i + '0');
 putchar(j + '0');
+putchar(i + '0');
 if (i == 9)
 {
 continue;
