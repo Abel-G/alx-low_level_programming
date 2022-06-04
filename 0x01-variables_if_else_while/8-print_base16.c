6631f9298c383e5ea1db77e37cb588c250f235f4
Abel <abeljeb@gmail.com>
2022-06-04 08:05:22 -0700

    prinitng base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+* Description: Printing base 16 in lowercase
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int i;
+char j;
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+for (j = 'a'; j <= 'f'; j++)
+{
+putchar(j);
+}
+putchar('\n');
+return (0);
+}
