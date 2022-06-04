1098e518aec0329dd793a34ad1b6481ad1c0d106
Abel <abeljeb@gmail.com>
2022-06-04 09:02:50 -0700

    two digit combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+* Description: printing two digit combo
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int i, j;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+putchar((i + ('0'))(j + ('0'));
+if (i == 9)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
