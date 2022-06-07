8bd7c6d3742145e8fc2444267b166c1aa37f1a6d
Abel <abeljeb@gmail.com>
2022-06-06 17:44:51 -0700

    commiting combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,19 +8,19 @@
 int main(void)
 {
 int i, j;
-for (i = 0; i < 9; i++)
+for (i = 0; i <= 9; i++)
 {
-j = i + 1;
-for (j <= 9; j++)
+for (j = 0; j <= 9; j++)
+{
+if (i < j){
 {
 putchar(i + '0');
 putchar(j + '0');
-if (i == 9)
+ if (i != 8 || (i == 8 && i != 9)
 {
-continue;
-}
 putchar(',');
 putchar(' ');
+}
 }
 }
 putchar('\n');
