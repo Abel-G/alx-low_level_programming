b6404d3923fbceb2c49948a8f46f274f5acdc43f
Abel <abeljeb@gmail.com>
2022-06-09 13:11:48 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,5 +7,12 @@
 */
 int print_last_digit(int y)
 {
-return (y % 10);
+int last_digit = y % 10;
+if (y < 0)
+{
+last_digit = -1 * last_digit;
+}
+_putchar(last_digit + '0');
+_putchar('\n');
+return (last_digit);
 }
