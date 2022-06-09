6cf83db7252c2e95e18a5301ec9f210e2ab71bb3
Abel <abeljeb@gmail.com>
2022-06-09 14:03:43 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,14 +5,16 @@
 * Return: void
 */
 void times_table(void)  
-{  
+{
+int x;
 int i;  
 for (i = 0; i <= 9; i++)    
 {      
 int j;      
 for (j = 0; j <= 9; j++)	
-{ 
-_putchar(i*j);  
+{
+x = i*j;
+_putchar(x + '0');  
 }
 _putchar('\n');    
 }
