89b673084b9df802a7dc40c3ebd22de7a4ec8d09
Abel <abeljeb@gmail.com>
2022-06-09 12:52:27 -0700

    absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,16 +2,17 @@
 /**
 * _abs - Entry point
 * Description: computing the absolute value
+* @x: integer it receives
 * Return: integer
 */
 int _abs(int x)
 {
 if (x >= 0)
 {
-return x;
+return (x);
 }
 else
 {
-return -(x);
+return (-(x));
 }
 }
