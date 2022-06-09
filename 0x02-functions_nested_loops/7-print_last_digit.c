c85bf2bc4758775775b503754920475e6ac3703c
Abel <abeljeb@gmail.com>
2022-06-09 13:00:42 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* print_last_digit - Entry point
+* Description: return the last digit
+* @y: the integer received
+* Return: integer
+*/
+int print_last_digit(int y)
+{
+return (y % 10);
+}
