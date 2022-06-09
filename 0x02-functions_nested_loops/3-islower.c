a4f5e945f1ce3314cfcbb610e6246f71c9f20f49
Abel <abeljeb@gmail.com>
2022-06-09 12:08:19 -0700

    push checker

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 /**
 * _islower - Entry point
 * Description: checks whether alphabet is lower case
-* @C: the integer value it recieves
+* @c: the integer value it recieves
 * Return:0 if false and 1 if tru
 */
 int _islower(int c)
