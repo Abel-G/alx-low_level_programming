9fb54e297b18fbdce98eb1c607e5e0fb5c7bd127
Abel <abeljeb@gmail.com>
2022-06-09 12:03:28 -0700

    print 1 if lower 0 else

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _islower - Entry point
+* Description: checks whether alphabet is lower case
+* Return:0 if false and 1 if tru
+*/
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
