ee68ac3d435d52eea117a1027b5ffd6a53fe1270
Abel <abeljeb@gmail.com>
2022-06-09 11:07:00 -0700

    printing alpahbets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - printing lower case alphabets
+ * Return: Always 0
+*/
+int main(void)
+{
+int i = 'a';
+while(i <= 'z')
+{
+_putchar(i);
+ i++;
+}
+_putchar('\n');
+ return (0);
+}
