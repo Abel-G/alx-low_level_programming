2a8778d08457cfad6fd23682e023ac80b7caaf2d
Abel <abeljeb@gmail.com>
2022-06-10 08:06:07 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
-* print_numbers - Entry point
+* print_numbers - print int 0 -9
 * Description: print int numbers
 * Return: void
 */
 void print_numbers(void)
 {
-int i = 0;
+char i = 0;
 while (i <= 9)
 {
-_putchar(i);
+_putchar(i + '0');
 i++;
 }
 _putchar('\n');
