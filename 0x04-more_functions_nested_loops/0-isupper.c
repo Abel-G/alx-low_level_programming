1b08533c4bf203852360058aa173853785d4e984
Abel <abeljeb@gmail.com>
2022-06-10 07:04:54 -0700

    check upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _isupper - Entry point
+* @c: integer received
+* Description: checks uppercase chx
+* Return: 1 if upper case 0 otherwise
+*/
+int _isupper(int c)
+{
+if (c >= 65 && c <= 97)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
