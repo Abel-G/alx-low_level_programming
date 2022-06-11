a1c3062142a87927644201936529c7041ed27870
Abel <abeljeb@gmail.com>
2022-06-11 06:31:07 -0700

    print int

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_numbers - print 0 - 9
+* Return: void
+*/
+void print_number(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+_putchar(i + '0');
+}
+_putchar('\n');
+}
