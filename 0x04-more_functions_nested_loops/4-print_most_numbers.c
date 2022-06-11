3209375bd771e89c4b4488b0b900d87b758e92a0
Abel <abeljeb@gmail.com>
2022-06-11 07:15:36 -0700

    Print

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,6 +2,7 @@
 /**
 * print_most_numbers - print 0 - 9 except 2 and 4
 * Return: void
+* Description: prints
 */
 void print_most_numbers(void)
 {
@@ -9,10 +10,13 @@ int i = 0;
 while (i <= 9)
 {
 if ( i == 2 || i == 9)
+{
 continue;
+}
 else
+{
 _putchar(i + '0');
+}
 }
 _putchar('\n');
 }
-	
