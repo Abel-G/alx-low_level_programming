6256a419663d2677685bd3aebb982dd9d1b83cd7
Abel <abeljeb@gmail.com>
2022-06-11 08:12:15 -0700

    print

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
-* more_numbers - print more numbers
-* Description: print numbers 0 - 14
+* more_numbers -  print numbers 0 - 14
 * Return: void
 */
 void more_numbers(void)
@@ -18,10 +17,9 @@ if (x > 9)
 _putchar('1');
 y = x % 10;
 }
-_putchar('0'+ c);
+_putchar('0' + c);
 }
 _putchar('\n');
 i++;
 }
 }
-	      
