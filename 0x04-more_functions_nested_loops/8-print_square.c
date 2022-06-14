183a31246f827643fa00e77a1338b69d88976d3d
Abel <abeljeb@gmail.com>
2022-06-13 22:56:26 -0700

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,17 +4,19 @@
 * @size: integer input
 * Return: void
 */
-void print_square(int square)
+void print_square(int size)
 {
 int i = 0
 int j;
-while (i < n && n > 0)
+while (i < size && size > 0)
 {
-for (j = 0; j < n; j ++)
+for (j = 0; j < size; j++)
 {
 _putchar('#');
 }
 _putchar('\n');
 i++;
 }
+if (i == 0)
+_putchar('\n');
 }
