fc2e1fee18d9dca64e739c763efd6e0330a8f84e
Abel <abeljeb@gmail.com>
2022-06-14 06:59:32 -0700

    print times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,33 +6,31 @@
 */
 void times_table(void)  
 {
-int x, y, z;
-int i;  
-for (i = 0; i <= 9; i++)    
-{      
-int j;      
-for (j = 0; j <= 9; j++)	
+int x, y, z, i, j;  
+for (x = 0; x <= 9; x++)    
+{            
+for (y = 0; y <= 9; y++)	
 {
-x = i*j;
-y = x / 10;
-z = x % 10;
-if (j == 0)
+z = x * y;
+i = z / 10;
+j = z % 10;
+if (y == 0)
 {
 _putchar('0');
 }
-else if (x < 10)
+else if (z < 10)
 {   
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
-_putchar(z +'0');
+_putchar(j + '0');
 }
 else
 {
 _putchar(',');
 _putchar(' ');
-_putchar(y + '0');
-_putchar(x + '0');
+_putchar(i + '0');
+_putchar(j + '0');
 }
 }
 _putchar('\n');    
