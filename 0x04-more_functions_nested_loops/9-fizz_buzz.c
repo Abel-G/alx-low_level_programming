67beb80671dbe9c18aab2b01c028ff7f985d8935
Abel <abeljeb@gmail.com>
2022-06-14 04:28:52 -0700

    print fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,22 +11,22 @@ for (i = 1; i <= 100; i++)
 if (i % 3 == 0)
 {
 printf("Fizz");
-printf(' ');
 }
 else if (i % 5 == 0)
 {
 printf("Buzz");
-printf(' ');
 }
 else if ((i % 3 == 0) && (i % 5 == 0))
 {
 printf("FizzBuzz");
-printf(' ');
 }
 else
 {
-printf(i);
-printf(' ');
+printf("%i", i);
+}
+if (i != 100)
+{
+putchar(' ');
 }
 }
 putchar('\n');
