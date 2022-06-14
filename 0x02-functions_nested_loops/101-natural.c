dacc2dd49982f39e5496c793547e1408e9813e09
Abel <abeljeb@gmail.com>
2022-06-14 07:16:46 -0700

    print natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: print sum of natural numbers
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int x = 0, i;
+while ((i % 3 == 0 || i % 5 == 0 ) && i < 1024)
+{
+x += i;
+i++;
+}
+printf("%d\n", x);
+}
