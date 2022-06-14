97188f501f3f34f556e05ca434907cbc391011c3
Abel <abeljeb@gmail.com>
2022-06-14 04:44:45 -0700

    print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+* print_trinagle -  print triangle
+* @n: integer input
+* Return: void
+*/
+void print_triangle(int n)
+{
+int i = i;
+int j;
+while (i <= n && n > 0)
+{
+j = 0;
+while (j < n - 1)
+{
+_putchar(' ');
+j++;
+}
+j = 0;
+while (j < i)
+{
+_putchar('#');
+j++;
+}
+_putchar('\n');
+i++;
+}
+if (i == 1)
+{
+_puthcar('\n');
+}
+}
