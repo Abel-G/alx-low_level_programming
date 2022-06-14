ff683c8e99a5afbba26d921b7f1c370081f44178
Abel <abeljeb@gmail.com>
2022-06-14 07:47:44 -0700

    fibonacci 2

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - main entry
+* Description; calculate fibonacci
+* Return: 0
+*/
+int main(void)
+{
+int x = 0;
+int y = 1;
+int z = 0;
+int i = 0;
+while (z < 4000000)
+{
+z = x + y;
+x = y;
+y = z;
+if (z % 2 == 0)
+i += z;
+}
+printf("%i\n", i);
+return (0);
+}
+  
