c04648a3a518e4e897553c36de5e031679c3bdd6
Abel <abeljeb@gmail.com>
2022-06-15 09:11:22 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.c"
+/**
+* _puts - print a string
+* @str: input char
+* Return: void
+*/
+void _puts(char *str)
+{
+int i = 0;
+while (str[i])
+{
+putchar(str[i]);
+i++;
+}
+putchar('\n');
+}
