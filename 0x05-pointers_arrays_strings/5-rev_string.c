0679af38826fd25760b0d696dd997d444a2a3a6f
Abel <abeljeb@gmail.com>
2022-06-15 09:40:29 -0700

    print reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* rev_string - prints reverse string
+* @s: string pointer
+* Return: 0
+*/
+void rev_string(char *s)
+{
+int x, y, z;
+char c;
+for (x = 0; s[x] != '\0'; x++)
+{
+y = 0;
+z = x / 2;
+}
+while (z--)
+{
+c = s[x - y - 1];
+s[x - y - 1] = s[y];
+s[y] = c;
+y++;
+}
+}
+      
