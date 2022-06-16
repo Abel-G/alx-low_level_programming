c52c64a3ba66269635d809159abedb1618af6e5b
Abel <abeljeb@gmail.com>
2022-06-16 05:14:29 -0700

    print array elements

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,12 +9,13 @@
 void print_array(int *a, int n)
 {
 int i;
-for (i = 0; i <= n; i++)
+for (i = 0; (i < n && n >= 0); i++)
 {
 printf("%d", a[i]);
-if (n > 0)
+if (n > 0 && i != n)
 {
 printf(", ");
 }
 }
+printf("\n");
 }
