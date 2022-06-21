d6fdf8c9f7dc89bb7a1644abd980549c6930cc23
Abel <abeljeb@gmail.com>
2022-06-21 06:41:25 -0700

    concatinate

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,9 +3,10 @@
 * *_strcat - concatenates @src to @dest
 * @src: the string to be merged
 * @dest: the string to be get appendednt
+* @n: number of bytes
 * Return: merged string
 */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 int x = 0, y = 0;
 while (dest[x++])
