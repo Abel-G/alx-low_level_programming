b7266a8775142b3543da830188813d6666a7f7cf
Abel <abeljeb@gmail.com>
2022-06-22 07:33:41 -0700

    comparing strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _strcmp - compair two strings
+* @s1: first string pointer
+* @s2: second string pointer
+* Return: differnce
+*/
+int _strcmp(char *s1, char *s2)
+{
+while ((*s1 && *s2) && (*s1 == *s2))
+s1++;
+s2++;
+return (*s1 - *s2);
+}
