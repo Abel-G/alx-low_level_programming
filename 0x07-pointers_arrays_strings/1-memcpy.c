f2867d1f5f78eb985a310bfda83cd67b653c895e
Abel <abeljeb@gmail.com>
2022-06-21 17:17:57 -0700

    print

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-* *_memcpy - copies n byte formmemory
+* *_memcpy - copies memory area
 * @dest: memory
 * @src: source
 * @n length
 * Return: pointer
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
-{ 
+{
 unsigned int i;
 for (i = 0; i < n; i++)
 {
