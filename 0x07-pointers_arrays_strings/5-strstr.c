8eb27cc1082d9fb20e76dc5b2008bfe7d1997bb0
Abel <abeljeb@gmail.com>
2022-06-24 18:33:57 -0700

    print

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,12 +6,15 @@
 * Return: substring or null
 */
 char *_strstr(char *haystack, char *needle)
+{
+int x;
 if (*needle == 0)
 return (haystack);
-int x;
 while (*haystack)
+{
 x = 0;
 if (haystack[x] == needle[x])
+{
 do {
 if (needle[x + 1] == '\0')
 return (haystack);
