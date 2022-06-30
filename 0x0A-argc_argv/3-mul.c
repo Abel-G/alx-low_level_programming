d2fde0c9313459143b7718924f64542cef65d2bd
Abel <abeljeb@gmail.com>
2022-06-30 13:26:47 -0700

    print

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - program that multiplies two num
+* @argc: number of arguments
+* @argv: array name
+* return: int
+*/
+int main(int argc, char *argv[])
+{
+int x, y;
+if (argc == 1 || argc == 2)
+{
+printf("error\n");
+return (1);
+}
+else
+{
+y = 1;
+for (x = 1; x < 3; x++)
+y *=atoi(argv[x]);
+printf("%d\n", y);
+}
+return (0);
+}
