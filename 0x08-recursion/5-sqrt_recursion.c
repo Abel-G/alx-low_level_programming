6aa9c3c7a87ce66036423af9153c9c990dfd0a28
Abel <abeljeb@gmail.com>
2022-06-30 10:50:12 -0700

    print

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-int find_sqrt(int num, int root);
+int find_sqrt(int x, int y);
 int _sqrt_recursion( int n);
 /**
 * find_sqrt - find square root of a number
@@ -7,13 +7,13 @@ int _sqrt_recursion( int n);
 * @root: root to be tested
 * Retrun: the square root or -1
 */
-int find_sqrt(int num, int root)
+int find_sqrt(int x, int y)
 {
-if ((root * root) == num)
-return (root);
-if (root == num / 2)
+if ((y * y) == x)
+return (y);
+if (y == x / 2)
 return (-1);
-return (find_sqrt(num, root +1));
+return (find_sqrt(x, y +1));
 }
 /**
 * _sqrt_recursion - Returns the square root number
@@ -22,9 +22,10 @@ return (find_sqrt(num, root +1));
 */
 int _sqrt_recursion(int n)
 {
+int y = 0;
 if (n < 0)
 return (-1);
 if (n == 1)
 return (1);
-return (find_sqrt(n, root));
+return (find_sqrt(n, y));
 }
