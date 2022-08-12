07ef20cc6786c2e1a874e2c21fd405edad1cc18a
Abel <abeljeb@gmail.com>
2022-08-12 05:51:34 -0700

    print function pointer q0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+* print_name - print name
+* @name: name for print
+* @f: pointer to function
+*/
+void print_name(char *name, void (*f)(char*))
+{
+if (name == NULL || f == NULL)
+return;
+f(name);
+}
