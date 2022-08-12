318f0d6dc7d34d70eb0fcb01875c6d2252ba66f8
Abel <abeljeb@gmail.com>
2022-08-12 06:28:58 -0700

    print

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include "function_pointers.c"
+/**
+* int_index - looks for an integer in array
+* @array: array of integers
+* @size: size of array
+* @cmp: pointer to funcction
+* Return: -1 or 0
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int index;
+if (array == NULL || cmp == NULL)
+return (-1);
+for (index = 0; index < size; index++)
+{
+if (cmp(array[index]) != 0)
+return (index);
+}
+return (-1);
+}
