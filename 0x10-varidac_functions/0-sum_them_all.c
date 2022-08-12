6c9ce7cd88946e34c315a010176e89b3e33a4792
Abel <abeljeb@gmail.com>
2022-08-12 08:13:43 -0700

    print

--- 0x10-varidac_functions/0-sum_them_all.c
+++ 0x10-varidac_functions/0-sum_them_all.c
@@ -9,10 +9,10 @@
 int sum_them_all(const unsigned int n, ...)
 {
 va_list num;
-unsigned int i, s =0;
+unsigned int i, s = 0;
 va_start(num, n);
-for(i = 0; i < n; i++)
+for (i = 0; i < n; i++)
 s += va_arg(num, int);
 va_end(num);
-return(s);
+return (s);
 }
