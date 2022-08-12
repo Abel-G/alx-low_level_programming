3749ae8b4899bc2b6e22c21412fd61910243d456
Abel <abeljeb@gmail.com>
2022-08-12 06:08:25 -0700

    print

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,7 +3,7 @@
 * array_iterator - executes a function given as a parameter of an array
 * @array: input array
 * @size: size of the array
-* @action: pointer to the function 
+* @action: pointer to the function
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 if (array == NULL || action == NULL)
