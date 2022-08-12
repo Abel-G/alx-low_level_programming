099bcced6f4d6f7c5435a40f168c34e5acbae353
Abel <abeljeb@gmail.com>
2022-08-12 04:27:19 -0700

    print

---
+++ 0x0E-structures_typedef/1-int_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+* int_dog - define new dog type struct
+* @d: poointer to a struct
+* @name: pointer to dog name
+* @age: pointer to dog's age
+* @owner: pointer to dog's owner
+* Return: void
+*/
+void int_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != 0)
+{
+d -> name = name;
+d -> age = age;
+d -> owner = owner;
+}
+}
