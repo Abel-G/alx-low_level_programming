9fe2835ba48ef97647d518605426e1e4dfe9546e
Abel <abeljeb@gmail.com>
2022-08-19 11:31:26 -0700

    print

---
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* binary_to_unit - convert binary in to unsigned int
+* @b: binary char to be converted
+* Return: converted num | 0
+*/
+unsigned int  binary_to_unit(const char *b)
+{
+unsigned int n = 0;
+int i = 0;
+if (b[i] == '\0')
+return (0);
+while ((b[i] == '0') || (b[i] == '1'))
+{
+n <<= 1;
+n += b[l] - '0';
+l++;
+}
+return (n);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+this project is about bitwise and bitwise manipulation
