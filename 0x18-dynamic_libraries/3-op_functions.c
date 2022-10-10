4f661128bd20bcb5e73bdf38e51d1c7db8a4435c
Abel-G <56924148+Abel-G@users.noreply.github.com>
2022-10-10 20:32:54 +0300

    print

--- 0x18-dynamic_libraries/3-op_functions.c
+++ 0x18-dynamic_libraries/3-op_functions.c
@@ -1,54 +1,51 @@
+include "main.h"
 /**
- * add - add
- * @a: integer
- * @b: integer
- * Return: result
- */
+* add - add
+* @a: integer
+* @b: integer
+* Return: result
+*/
 int add(int a, int b)
 {
-	return (a + b);
+return (a + b);
 }
-
 /**
- * sub - subtract
- * @a: integer
- * @b: integer
- * Return: result
- */
+* sub - subtract
+* @a: integer
+* @b: integer
+* Return: result
+*/
 int sub(int a, int b)
 {
-	return (a - b);
+return (a - b);
 }
-
 /**
- * mul - multiply
- * @a: integer
- * @b: integer
- * Return: result
- */
+* mul - multiply
+* @a: integer
+* @b: integer
+* Return: result
+*/
 int mul(int a, int b)
 {
-	return (a * b);
+return (a * b);
 }
-
 /**
- * div - divide
- * @a: integer
- * @b: integer
- * Return: result
- */
+* div - divide
+* @a: integer
+* @b: integer
+* Return: result
+*/
 int div(int a, int b)
 {
-	return (a / b);
+return (a / b);
 }
-
 /**
- * mod - modulus for remainder
- * @a: integer
- * @b: integer
- * Return: result
- */
+* mod - modulus for remainder
+* @a: integer
+* @b: integer
+* Return: result
+*/
 int mod(int a, int b)
 {
-	return (a % b);
+return (a % b);
 }
