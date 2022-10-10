1cdcd19fea191acef89b1d053ddffa2d3ba79370
Abel-G <56924148+Abel-G@users.noreply.github.com>
2022-10-10 20:39:02 +0300

    print

--- 0x18-dynamic_libraries/6-abs.c
+++ 0x18-dynamic_libraries/6-abs.c
@@ -1,17 +1,14 @@
 #include "main.h"
-
 /**
- * _abs - compute absolute value of integer
- * @n: integer argument
- * Return: absolute value
- */
-
+* _abs - compute absolute value of integer
+* @n: integer argument
+* Return: absolute value
+*/
 int _abs(int n)
 {
-	if (n > 0)
-		return (n);
-	else if (n < 0)
-		return (-n);
-
-	return (0);
+if (n > 0)
+return (n);
+else if (n < 0)
+return (-n);
+return (0);
 }
