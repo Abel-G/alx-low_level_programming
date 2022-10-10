17710151c26ea02d14e6e71d5e486d411d47ff16
Abel-G <56924148+Abel-G@users.noreply.github.com>
2022-10-10 20:29:25 +0300

    print

--- 0x18-dynamic_libraries/2-strlen.c
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -1,20 +1,16 @@
 #include "main.h"
-
 /**
- * _strlen - returns the length of a string
- * @s: string
- * Return: length
- */
-
+* _strlen - returns the length of a string
+* @s: string
+* Return: length
+*/
 int _strlen(char *s)
 {
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-
-	return (len);
+int len = 0;
+while (*s != '\0')
+{
+len++;
+s++;
+}
+return (len);
 }
