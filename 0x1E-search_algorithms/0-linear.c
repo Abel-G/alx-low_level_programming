6e55b5881a06b349b368ef523c5152279c5fc05a
Abel-G <abeljeb@gmail.com>
2023-05-13 07:16:46 -0700

    print

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,4 @@
-#include "search_algos.h"
+0#include "search_algos.h"
 
 /**
 * linear_search - searches for a value in an array of integers using
@@ -10,14 +10,15 @@
 * Return: first index where value is located, (-1) if array is NULL or
 */
 
-int linear_search(int *array, size_t size, int value) {
-if (array == NULL) {
-return -1;
-}
-for (size_t i = 0; i < size; i++) {
+int linear_search(int *array, size_t size, int value)
+{
+if (array == NULL)
+return (-1);
+for (size_t i = 0; i < size; i++)
+{
 printf("Comparing %d to %d\n", array[i], value);
-if (array[i] == value) {
-return i;
+if (array[i] == value)
+return (i);
 }
+return (-1);
 }
-return -1
