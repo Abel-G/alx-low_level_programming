4ebb73d8a58198a1e5f59b3e871163137274942e
Abel-G <abeljeb@gmail.com>
2023-05-13 07:37:36 -0700

    print

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,11 +9,11 @@
 * @value: value to search for in array
 * Return: first index where value is located, (-1) if array is NULL or
 */
-int i;
 int linear_search(int *array, size_t size, int value)
 {
 if (array == NULL)
 return (-1);
+int i;
 for (i = 0; i < (int)size; i++)
 {
 printf("Value checked array[%d] = [%d]\n", i,  array[i]);
